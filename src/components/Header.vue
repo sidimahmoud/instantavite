927602122c0525ba8e8b455ae04be046d27faff3
Mokhtar sidi mahmoud <sidmah10@gmail.com>
2020-07-11 18:47:43 +0000

    app changes on header small

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,11 +15,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="search-item">
-      <el-input placeholder="Chercher un produit par nom" v-model="product">
-        <span class="el-icon-search el-input__icon app-cursor-pointer" slot="suffix" ></span>
-      </el-input>
-    </div>  
+     
 
     <div class="collapse navbar-collapse" id="navbarTop" :class="{show: isNavOpen}">
       <ul class="navbar-nav mr-auto">
