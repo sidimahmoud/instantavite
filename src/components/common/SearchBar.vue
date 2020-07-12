986e1e0d0d931cbdd783669920837eddc97b9535
Mokhtar sidi mahmoud <sidmah10@gmail.com>
2020-07-12 15:13:38 +0000

    app changes on header margin top

--- src/components/common/SearchBar.vue
+++ src/components/common/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container-fluid bg-light text-dark">
+    <div class="container-fluid bg-light text-dark" style="margin-top:5px;">
         <div class="search-item">
             <el-input placeholder="Chercher un produit par nom" v-model="product">
             <span class="el-icon-search el-input__icon app-cursor-pointer" slot="suffix" ></span>
